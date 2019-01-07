0a57216e97256bceba292003c99d23fec14840fa
localpunter <localpunter@sky.com>
2019-01-07 18:05:11 +0000

    Arrays, polymorphism and inheritance updated in PDA

---
+++ pda.rb
@@ -0,0 +1,78 @@
+
+def array_sort
+    names  = [
+        "Alan",
+        "Rachel",
+        "Liam",
+        "Claire",
+        "Scott",
+        "Charlie",
+        "Alison",
+        "Aaron",
+        "Rudi"
+    ]
+    result = names.sort
+
+    puts result
+    
+end
+
+array_sort
+
+
+
+# def array_search
+#     names  = [
+#         "Alan",
+#         "Rachel",
+#         "Liam",
+#         "Claire",
+#         "Scott",
+#         "Charlie",
+#         "Alison",
+#         "Aaron",
+#         "Rudi"
+#     ]
+#     result = names.find_all { |name| name[0] == "A"}
+
+#     puts result
+    
+# end
+
+# array_search
+
+
+# def array_sort
+#     names  = [
+#         "Alan",
+#         "Rachel",
+#         "Liam",
+#         "Claire",
+#         "Scott",
+#         "Charlie",
+#         "Alison",
+#         "Aaron",
+#         "Rudi"
+#     ]
+#     result = names.sort { |left, right| left.length <=> right.length}
+
+#     puts result
+    
+# end
+
+# array_sort
+
+
+
+# def display_persons_name
+#     people = [
+#         {name: "Alan", gender: "Male"},
+#         {name: "Rachel", gender: "Female"},
+#         {name: "Liam", gender: "Male"},
+#         {name: "Claire", gender: "Female"}
+#     ]
+    
+#     people.each { |person| puts person[:name] }
+# end
+
+# display_persons_name
